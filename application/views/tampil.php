e5cb8abb3d20ed035c155003ffd32cbf660a3abb
uvalmy <naufalmyajid10@gmail.com>
2022-09-28 15:45:21 +0700

    latihan-ci

---
+++ application/views/tampil.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Latihan Pertama</title>
+</head>
+<body>
+    <p>Latihan Pertama saya Belajar framework Codeigniter<p>
+    <p>Framework Codeigniter sangat mudah dipelajari<p>
+        
+</body>
+</html>
